7176133a5f05911f24ced8a62ae3fba1a12aef7e
GeraOR <146026356+GeraOR@users.noreply.github.com>
2024-06-23 15:00:04 -0600

    Add files via upload

--- JS/in.js
+++ JS/in.js
@@ -22,10 +22,10 @@ actualizarContador();
 
 // BOTONES
 function enterWe(){
-    window.location.href = "./html/we.html";
+    window.location.href = "/html/we.html";
 }
 function enterPlaces(){
-    window.location.href = "./html/places.html";
+    window.location.href = "/html/places.html";
 }
 function back(){
     window.location.href = "/index.html"
